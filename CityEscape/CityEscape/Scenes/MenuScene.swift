461e65f27b42dee5fd0845b6eb8d0dc611f20cb2
Soumyadeep Chatterjee <soumyadeepchatterjee803@gmail.com>
2025-02-20 09:09:25 -0500

    menu btn fix

--- CityEscape/CityEscape/Scenes/MenuScene.swift
+++ CityEscape/CityEscape/Scenes/MenuScene.swift
@@ -17,43 +17,46 @@ class MenuScene: SKScene {
     }
     
     func setupUI() {
-        let charactersButton = createButton(size: CGSize(width:350, height: 100), text: "Characters")
-        charactersButton.position = CGPoint(x:size.width/2, y:size.width/2 + 50)
+        let buttonSpacing: CGFloat = 120  
+
+        let charactersButton = createButton(size: CGSize(width: 350, height: 100), text: "Characters")
+        charactersButton.position = CGPoint(x: size.width / 2, y: size.height / 2 + buttonSpacing)
         charactersButton.name = "charactersButton"
         addChild(charactersButton)
-        
+
         let soundButton = createButton(size: CGSize(width: 300, height: 80), text: "Sound")
-        soundButton.position = CGPoint(x: size.width / 2, y: size.height / 2 - 100)
+        soundButton.position = CGPoint(x: size.width / 2, y: size.height / 2)
         soundButton.name = "soundButton"
         addChild(soundButton)
-        
+
         let backButton = createButton(size: CGSize(width: 150, height: 60), text: "Back")
-        backButton.position = CGPoint(x: size.width / 2, y: 100)
+        backButton.position = CGPoint(x: size.width / 2, y: size.height / 2 - buttonSpacing)
         backButton.name = "backButton"
         addChild(backButton)
     }
+
     
     func createButton(size: CGSize, text: String) -> SKSpriteNode {
-        let button = SKSpriteNode(color:.red, size: size)
+        let button = SKSpriteNode(color: .red, size: size)
         button.zPosition = 1
-        
-        let border = SKShapeNode(rectOf: size, cornerRadius: 10)
-        border.strokeColor = .white
-        border.lineWidth = 6
-        border.zPosition = button.zPosition + 1
-        button.addChild(border)
-        
+
         let label = SKLabelNode(fontNamed: "AvenirNext-Bold")
         label.text = text
-        label.fontSize = size.height / 2.5
+        label.fontSize = min(size.height / 3, 40)
         label.fontColor = .black
         label.verticalAlignmentMode = .center
         label.zPosition = button.zPosition + 1
         button.addChild(label)
-        
+
+        let border = SKShapeNode(rectOf: size, cornerRadius: 10)
+        border.strokeColor = .white
+        border.lineWidth = 6
+        border.zPosition = button.zPosition + 2
+        button.addChild(border)
+
         return button
     }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             let location = touch.location(in: self)
